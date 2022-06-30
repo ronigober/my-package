980ca41011b2329b947218fb0c5db77f68e4a580
Kim <kimb@r-stealth.com>
2022-06-30 11:22:58 +0300

    Demo

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = ' '
+
+setup(
+   name="my-package",
+   version=VERSION,
+   author="Kim Boren",
+   author_email="kimb@r-stealth.com",
+   description=DESCRIPTION,
+   packages=find_packages(),
+   install_requires=["sourcedefender", "pandas"],
+   setup_requires=['setuptools'],
+   python_requires='>=3.9.0'
+)
